bd7da37c801a3cf534439681ca15a3a587d465ee
Dc-cpu-arch <d.garzong77@gmail.com>
2020-06-11 02:20:53 +0000

    file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *  main - prints "Programming is like building a multilingual puzzle
+ *
+ *
+ *  Return: Always 0
+ */
+int main(void)/*main -*/
+{
+puts("\"Programming is like buiding a multilingual puzzle");
+return (0);
+}
