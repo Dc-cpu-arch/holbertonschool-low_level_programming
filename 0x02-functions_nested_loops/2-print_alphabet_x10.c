0667dcbd06f353b6549c1cc5442579d3ed18e8b2
Dc-cpu-arch <d.garzong77@gmail.com>
2020-06-18 01:00:45 +0000

    file 2 updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,33 +1,32 @@
 #include "holberton.h"
-
 /**
  *print_alphabet_x10 - prints the alphabet 10 times
  *
- *print_alphabet -  prints alphabet x1
  *
  *Return: Always 0 - ok
  */
-
 void print_alphabet_x10(void)
 {
 	int n = 0;
 
 	while (n < 10)
 	{
-		print_alphabet_once();
+		print_alphabet();
 		n++;
 	}
-
 }
+/**
+ *print_alphabet - prints the alphabet once
+ */
 
 void print_alphabet(void)
 {
 	char c = 'a';
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c = c + 1;
-	}
-	putchar('n');
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c = c + 1;
+		}
+	_putchar('\n');
 }
