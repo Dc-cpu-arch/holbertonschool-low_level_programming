05b246d6525e0856745b2d7ead2a28692c455164
Dc-cpu-arch <d.garzong77@gmail.com>
2020-06-24 19:30:28 +0000

    file 7 uptodate

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puths_half - prints half of a string.
+ * @str: string with 10 numbers (0 to 9).
+ * Return: Null.
+ */
+
+void puts_half(char *str)
+{
+	int n, i;
+
+	while (str[n])
+	{
+		n++;
+	}
+
+	if (n % 2 == 1)
+	{
+		i = (n - 1) / 2;
+		i = n - i;
+	}
+
+	else
+	{
+		i = n / 2;
+	}
+
+	for (; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
