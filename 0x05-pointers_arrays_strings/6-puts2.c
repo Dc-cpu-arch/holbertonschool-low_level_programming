e73ebf152bc5d6bc733b2d55c22c362fd3bdc435
Dc-cpu-arch <d.garzong77@gmail.com>
2020-06-24 18:41:25 +0000

    file 6 updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,15 +11,13 @@ void puts2(char *str)
 	int i;
 
 	while (str[i])
-
+	{
+		if (i % 2 == 0)
 		{
-			if (i % 2 == 0)
-			{
-				_putchar(str[i]);
-				}
+			_putchar(str[i]);
+		}
 
 		i++;
-
-		}
+	}
 	_putchar('\n');
 }
