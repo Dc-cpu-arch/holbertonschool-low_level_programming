77d059f996cadfe5dda70677f766f6ce2bfa76eb
Dc-cpu-arch <d.garzong77@gmail.com>
2020-06-25 00:21:47 +0000

    files 8  & 9 first upload

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to src, including Null.
+ *@src: source string.
+ *@dest: destination string.
+ * Return: destination pointer.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
