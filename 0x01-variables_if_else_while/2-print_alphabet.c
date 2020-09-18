8b9d64ec5b4235fee25af96dc7f00a0cf3f5f8f5
Dc-cpu-arch <d.garzong77@gmail.com>
2020-09-18 17:09:27 +0000

    file 2-print_alphabet.c is working properly

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
-char a;
-a = 'a';
-while (a <= 'z')
-{
-putchar(a);
-a = a + 1;
-}
-putchar('\n');
-return (0);
+	char a = 97;
+
+	for (; a <= 122; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
 }
