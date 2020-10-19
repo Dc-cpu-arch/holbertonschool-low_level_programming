8799a2aa06a73c21b6870567d7f3d01412826941
Dc-cpu-arch <d.garzong77@gmail.com>
2020-10-19 02:35:51 +0000

    file 0-malloc_checked.c first update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: does the same as size_t in the original malloc
+ * Return: pointer with allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pointer = NULL;
+
+	pointer = malloc(b);
+
+	if (!pointer)
+		exit(98);
+
+	return (pointre);
+}
+
