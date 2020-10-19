91d63454483390931606bab758085bfd854b5676
Dc-cpu-arch <d.garzong77@gmail.com>
2020-10-19 00:22:27 +0000

    file holberton.h with all the prototypes

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,28 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+void simple_print_buffer(char *buffer, unsigned int size);
+
+void print_grid(int **grid, int width, int height);
+
+int main(int ac, char *av[]);
+
+int main(void);
+
+#endif /* _HOLBERTON_H_ */
