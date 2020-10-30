ebd5257cec6f205014d54157923df20cd65e50cf
Dc-cpu-arch <d.garzong77@gmail.com>
2020-10-30 20:06:58 +0000

    remastered

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * print_list -  prints the members of the nodes of a list and count them.
- * @h: pointer to the first node of the list.
+ * print_list - prints the members of the nodes of a list and counts them.
+ * @: pointer to the first node of the list.
  * Return: amount of nodes whitin the list.
  */
 size_t print_list(const list_t *h)
@@ -15,15 +15,12 @@ size_t print_list(const list_t *h)
 	for (nodes = 0; list != NULL; nodes++)
 	{
 		if (list->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%i] %s\n", list->len, list->str);
-		}
 
-		list = list->next;
+		else
+			printf("[%i] %s", list->len, list->str);
+
+	list = list->next;
 	}
 	return (nodes);
 }
