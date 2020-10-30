08c1c2516612c1cbbef0f0223be2d8659b4e82ef
Dc-cpu-arch <d.garzong77@gmail.com>
2020-10-30 02:14:15 +0000

    file 2*.c were fixed and some comments were added to better understanding

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a single linked list.
+ * @head: pointer to pointer to the first node of the list, the head.
+ * @str: main member of the node structure.
+ * Return: a pointer towards the newly created node.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i = 0;
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	new->len = i; /*1st member of the node structure*/
+	new->str = strdup(str);/*2nd member of the node structure*/
+	new->next = *head;/*3rd member of the node structure*/
+	/*The list is created from the bottom up*/
+	*head = new;
+
+	return (new);
+}
