78637486741e0a3d607f85dd31a16b13361bf9af
Dc-cpu-arch <d.garzong77@gmail.com>
2020-10-30 02:19:14 +0000

    advanced update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "list.h"
+
+/**
+ * print_after_main - prints in stdout before execution of main function.
+ **/
+void print_after_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
