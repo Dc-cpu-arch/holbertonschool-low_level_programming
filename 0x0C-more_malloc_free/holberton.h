d6f537d8bcdd452f58350d367738dc350fa665a9
Dc-cpu-arch <d.garzong77@gmail.com>
2020-11-02 17:43:55 +0000

    updated file

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -2,6 +2,7 @@
 #define _HOLBERTON_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 
@@ -14,5 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 
 char **strtow(char *str);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* _HOLBERTON_H_ */
