4c1089e87ff033a330516db600e785848244367c
Dc-cpu-arch <d.garzong77@gmail.com>
2020-11-06 02:22:20 +0000

    endianness

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,5 +17,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+int get_endianness(void);
 
 #endif /* HOLBERTON_H */
