b8678d9abfdd885d8fd6daa3bea2f19e47237ff6
Dc-cpu-arch <d.garzong77@gmail.com>
2020-11-10 03:57:58 +0000

    compiling and with style fix 0.2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file using file i/o and oflags.
+ * @filename: pointer to chars with the name of the given file.
+ * @text_content: pointer to chars with the characters to write in the file. 
+ *
+ * Return: 1 if succeeded, otherwise -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0;
+	int lenght = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[lenght])
+			lenght++;
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd < 0)
+		return (-1);
+
+	write(fd, text_content, lenght);
+
+	return (1);
+}
