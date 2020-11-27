fefa55dcad9e3741b3ad8c7f1512859573a70c63
Dc-cpu-arch <d.garzong77@gmail.com>
2020-11-27 03:23:26 +0000

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,9 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (count == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
-		con[i] = s1[i];
+		count[i] = s1[i];
 	for (j = 0; j < n; j++)
-		con[i++] = s2[j];
+		count[i++] = s2[j];
 	count[i] = '\0';
 	return (count);
 }
