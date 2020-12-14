4755d31bafcf8f47dc9b4d528565c4f9deb72318
Dc-cpu-arch <d.garzong77@gmail.com>
2020-12-14 04:27:05 +0000

    inserts at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *current = *h, new_node = NULL;
+	dlistint_t *current = *h, *new_node = NULL;
 
 	new_node = malloc(sizeof(dlistint_t));
 
