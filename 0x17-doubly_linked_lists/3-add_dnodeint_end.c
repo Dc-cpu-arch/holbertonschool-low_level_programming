09de66937f12859e205faa498d2f22600fc6449f
Dc-cpu-arch <d.garzong77@gmail.com>
2020-12-14 04:11:25 +0000

    adds a node at the end of a DLL

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a DDL
+ * @head: first node
+ * @n: data
+ *
+ * Return: address of the new node, or NULL if it fails
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+	dlistint_t *current = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	if (current)
+	{
+		while (current->next)
+		{
+			current = current->next;
+		}
+		new_node->prev = current;
+		current->next = new_node;
+	}
+	else
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+	}
+	return (new_node);
+}
