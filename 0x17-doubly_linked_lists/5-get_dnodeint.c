ced8349a497af1edf30c510979430ff5311170b1
Dc-cpu-arch <d.garzong77@gmail.com>
2020-12-14 04:16:39 +0000

    fetches a node and returns data

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - fetches a node in a DDL and returns its data
+ * @head: first node
+ * @index: nth node to be fetched
+ *
+ * Return: data -if node exists-, otherwise NULL.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+
+	while (index > 0)
+	{
+		current = current->next;
+		index--;
+		if (!current)
+			return (NULL);
+	}
+	return (NULL);
+}
